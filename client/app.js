aa3d4b5c0bc818bad9a27c4f7bc027ebee6c0a83
George Tudor <Alexander.George.Tudor@gmail.com>
2021-12-22 17:07:42 -0600

    search itunes api for author search, progress

--- client/app.js
+++ client/app.js
@@ -1,3 +1,16 @@
 const app = new Vue({
-
+    el: '#authorSearch',
+    data: () => ({
+        authorSearch: '',
+        results: [],
+        showBooks: false
+    }),
+    methods: {
+        search: function () {
+            const term = encodeURIComponent(this.authorSearch)
+            fetch(`https://itunes.apple.com/search?term=${term}&lang=en_us&media=ebook&entity=ebook&limit=500&attribute=authorTerm`)
+                .then(res => res.json())
+                .then(res => (this.results = res.results))
+        }
+    }
 })
