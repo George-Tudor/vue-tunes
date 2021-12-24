e297d94f6f78b8737516a82ae3f5f3e0bf4f90bc
George Tudor <Alexander.George.Tudor@gmail.com>
2021-12-23 23:58:10 -0600

    book search works, author search is now broken

--- client/app.js
+++ client/app.js
@@ -1,11 +1,9 @@
 const app = new Vue({
-    el: '#books', '#titles',
+    el: '#books',
     data: () => ({
         authorSearch: '',
         results: [],
-        showBooks: false,
-        titleSearch: '',
-        showTitle: false
+        showBooks: false
     }),
     methods: {
         search: function () {
@@ -13,12 +11,20 @@ const app = new Vue({
             fetch(`https://itunes.apple.com/search?term=${term}&lang=en_us&media=ebook&entity=ebook&limit=500&attribute=authorTerm`)
                 .then(res => res.json())
                 .then(res => (this.results = res.results))
-            },
+        }
+    },
+    el: '#titles',
+    data: () => ({
+        titleSearch: '',
+        results: [],
+        showTitle: false
+    }),
+    methods: {
         searchTitle: function () {
             const term = encodeURIComponent(this.titleSearch)
             fetch(`https://itunes.apple.com/search?term=${term}&media=ebook&entity=ebook&limit=50`)
                 .then(res => res.json())
                 .then(res => (this.results = res.results))
-        }
+                    }
     }
 })
