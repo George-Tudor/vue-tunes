1b594bfbfaf69461372d7a22d7ae85d79e77f255
George Tudor <Alexander.George.Tudor@gmail.com>
2021-12-23 23:54:48 -0600

    added book search feature and broke everything

--- client/app.js
+++ client/app.js
@@ -1,14 +1,22 @@
 const app = new Vue({
-    el: '#books',
+    el: '#books', '#titles',
     data: () => ({
         authorSearch: '',
         results: [],
-        showBooks: false
+        showBooks: false,
+        titleSearch: '',
+        showTitle: false
     }),
     methods: {
         search: function () {
             const term = encodeURIComponent(this.authorSearch)
             fetch(`https://itunes.apple.com/search?term=${term}&lang=en_us&media=ebook&entity=ebook&limit=500&attribute=authorTerm`)
+                .then(res => res.json())
+                .then(res => (this.results = res.results))
+            },
+        searchTitle: function () {
+            const term = encodeURIComponent(this.titleSearch)
+            fetch(`https://itunes.apple.com/search?term=${term}&media=ebook&entity=ebook&limit=50`)
                 .then(res => res.json())
                 .then(res => (this.results = res.results))
         }
