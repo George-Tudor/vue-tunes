debdbf3f87adde5ab498ec39b6ba3836a1ccaaba
George Tudor <Alexander.George.Tudor@gmail.com>
2021-12-25 23:50:11 -0600

    added search for itunes author name and id

--- client/app.js
+++ client/app.js
@@ -3,9 +3,11 @@ const app = new Vue({
     data: () => ({
         authorSearch: '',
         titleSearch: '',
+        searchAuthors: '',
         results: [],
         showBooks: false,
-        showTitle: false
+        showTitle: false,
+        showAuthors: false
     }),
     methods: {
         search: function () {
@@ -19,7 +21,14 @@ const app = new Vue({
             fetch(`https://itunes.apple.com/search?term=${titleTerm}&media=ebook&entity=ebook&limit=50`)
                 .then(res => res.json())
                 .then(res => (this.results = res.results))
+        },
+        searchAuthorId: function () {
+            const authorName = encodeURIComponent(this.searchAuthors)
+            fetch(`https://itunes.apple.com/search?term=${authorName}&media=ebook&entity=ebookAuthor&attribute=authorTerm&limit=20`)
+                .then(res => res.json())
+                .then(res => this.results = res.results)
+            }
         }
-    },
+
 
 })
